80b40a611274fb3c9f8c945da3c9dd598145056e
Laurent GAY <l.gay@sd-libre.fr>
2024-04-29 16:15:29 +0200

    build with version

--- version_build.sh
+++ version_build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 
+set -e
+
 rootversion="2.0.1"
 buildtstmp=$(git log -1 --format=%at)
-build_number=$(python -c "import datetime;print(datetime.datetime.fromtimestamp(int('${buildtstmp}')).strftime('%y%m%d%H'))")
+build_number=$(python3 -c "import datetime;print(datetime.datetime.fromtimestamp(int('${buildtstmp}')).strftime('%y%m%d%H'))")
 
 cd $(dirname $(readlink -f "$0"))
 mkdir -p public/conf
