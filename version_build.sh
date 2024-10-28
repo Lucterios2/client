ce02cca12b848495675d8616eb6bbe18a252625a
Laurent GAY <l.gay@sd-libre.fr>
2024-10-28 16:21:14 +0100

    version 2.0.3

--- version_build.sh
+++ version_build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rootversion="2.0.2"
+rootversion="2.0.3"
 buildtstmp=$(git log -1 --format=%at)
 build_number=$(python3 -c "import datetime;print(datetime.datetime.fromtimestamp(int('${buildtstmp}')).strftime('%y%m%d%H'))")
 
