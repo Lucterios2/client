23ce2d099c6278a645392663c868cec7d81f1e97
Laurent GAY <l.gay@sd-libre.fr>
2025-11-19 11:54:55 +0100

    nodejs version

--- version_build.sh
+++ version_build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rootversion="2.0.7"
+rootversion="2.0.8"
 buildtstmp=$(git log -1 --format=%at)
 build_number=$(python3 -c "import datetime;print(datetime.datetime.fromtimestamp(int('${buildtstmp}')).strftime('%y%m%d%H'))")
 
